fc68c9a7770bec801764f5dce6e67e2b0b4dca89
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2023-08-01 01:54:44 +0600

    Heading add

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
       <div class="w-full mt-5 rounded border-2">
         <div class="w-full h-10 -mt-[20px] px-1 flex justify-between items-center">
-          <span class="px-3 rounded-full bg-white text-gray-800 font-normal text-base">Heading</span>
+          <span class="px-3 rounded-full bg-gray-200 text-gray-800 font-normal text-base">Heading</span>
           <span class="p-1 rounded-full bg-white">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#e00" class="text-red"
               viewBox="0 0 16 16">
@@ -30,11 +30,82 @@
             </svg>
           </span>
         </div>
+        <div class="w-full mt-5 p-4">
+          <div class="w-full">
+            <input type="text" id="first_name"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              placeholder="Heading">
+          </div>
+          <div class="w-full mt-3 flex justify-between items-center">
+            <div class="inline-flex rounded-md shadow-sm" role="group">
+              <button 
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+                <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                  viewBox="0 0 20 20">
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                </svg>
+                Left
+              </button>
+              <button 
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+                <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+                </svg>
+                Center
+              </button>
+              <button 
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+                <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                  viewBox="0 0 20 20">
+                  <path
+                    d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
+                  <path
+                    d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
+                </svg>
+                Right
+              </button>
+            </div>
+            <div class="inline-flex rounded-md shadow-sm" role="group">
+              <button 
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+                <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                  viewBox="0 0 20 20">
+                  <path
+                    d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                </svg>
+                Blue
+              </button>
+              <button 
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+                <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                  viewBox="0 0 20 20">
+                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M4 12.25V1m0 11.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M4 19v-2.25m6-13.5V1m0 2.25a2.25 2.25 0 0 0 0 4.5m0-4.5a2.25 2.25 0 0 1 0 4.5M10 19V7.75m6 4.5V1m0 11.25a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM16 19v-2" />
+                </svg>
+                Black
+              </button>
+              <button 
+                class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 ">
+                <svg class="w-3 h-3 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                  viewBox="0 0 20 20">
+                  <path
+                    d="M14.707 7.793a1 1 0 0 0-1.414 0L11 10.086V1.5a1 1 0 0 0-2 0v8.586L6.707 7.793a1 1 0 1 0-1.414 1.414l4 4a1 1 0 0 0 1.416 0l4-4a1 1 0 0 0-.002-1.414Z" />
+                  <path
+                    d="M18 12h-2.55l-2.975 2.975a3.5 3.5 0 0 1-4.95 0L4.55 12H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-4a2 2 0 0 0-2-2Zm-3 5a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z" />
+                </svg>
+                Green
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div class="w-full mt-5 rounded border-2">
         <div class="w-full h-10 -mt-[20px] px-1 flex justify-between items-center">
-          <span class="px-3 rounded-full bg-white text-gray-800 font-normal text-base">Image</span>
+          <span class="px-3 rounded-full bg-gray-200 text-gray-800 font-normal text-base">Image</span>
           <span class="p-1 rounded-full bg-white">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#e00" class="text-red"
               viewBox="0 0 16 16">
@@ -43,11 +114,15 @@
             </svg>
           </span>
         </div>
+        
+        <div class="w-full mt-5 p-4">
+          <div class="w-full h-10 -mt-[20px] px-1 flex justify-between items-center"></div>
+        </div>
       </div>
 
       <div class="w-full mt-5 rounded border-2">
         <div class="w-full h-10 -mt-[20px] px-1 flex justify-between items-center">
-          <span class="px-3 rounded-full bg-white text-gray-800 font-normal text-base">Description</span>
+          <span class="px-3 rounded-full bg-gray-200 text-gray-800 font-normal text-base">Description</span>
           <span class="p-1 rounded-full bg-white">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#e00" class="text-red"
               viewBox="0 0 16 16">
@@ -62,8 +137,7 @@
     <div class="w-full h-[90vh] p-4 mt-4 rounded border-2 border-gray-200 "
       style="border-top-style: dotted; border-bottom-style: dotted">
       <div class="w-full h-10 -mt-[30px] flex items-center justify-center bg-white rounded text-center gap-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-          viewBox="0 0 16 16">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
           <path fill-rule="evenodd"
             d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z" />
           <path
